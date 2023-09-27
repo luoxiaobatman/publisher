529737b54de7fef92f86feac657e773a77b2c899
luo_xiao4 <luo_xiao4@venusgroup.com.cn>
2023-09-27 11:23:02 +0800

    solong

---
+++ build.sh
@@ -0,0 +1,12 @@
+VERSIONED=$1
+MKDOCS_CONFIG_FILE="mkdocs.yml"
+
+if [ -n "$VERSIONED" ]; then
+    MKDOCS_CONFIG_FILE=mkdocs_"$VERSIONED".yml
+fi
+
+mike deploy -F "$MKDOCS_CONFIG_FILE" -b main --prefix versioned_site --ignore "$VERSIONED"
+
+# git 魔法!
+git reset --hard HEAD^
+git reset --mixed HEAD^
